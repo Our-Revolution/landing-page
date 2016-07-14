88f3b69dd2eba5e34db86634aa70469b89a248bb
cjmabry <cjmab28@gmail.com>
2016-07-14 16:48:59 -0500

    Server fix

--- app.js
+++ app.js
@@ -68,5 +68,8 @@ app.use(function(err, req, res, next) {
     });
 });
 
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 module.exports = app;
--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ gulp.task('watch', function() {
 gulp.task('develop', function () {
   livereload.listen();
   nodemon({
-    script: 'bin/www',
+    script: 'app.js',
     ext: 'js handlebars coffee',
     stdout: false
   }).on('readable', function () {
