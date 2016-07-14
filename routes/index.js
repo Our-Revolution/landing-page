1f7aebeb53441553555c4f5bcd58c7d1c01037bc
cjmabry <cjmab28@gmail.com>
2016-07-14 18:25:45 -0500

    Started POST request

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Our Revolution' });
+});
+
+router.post('/', function(req, res) {
+  console.log(res.body);
+  res.render('index', { title: 'Our Revolution' });
 });
 
 module.exports = router;
