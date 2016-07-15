1e7cc25b1f0e34684f7b731c540e579cd7407011
cjmabry <cjmab28@gmail.com>
2016-07-14 21:35:27 -0500

    db action

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
 
 router.get('/', function(req, res) {
 res.render('index', {
@@ -12,8 +13,8 @@ res.render('index', {
 });
 
 router.post('/', function(req, res) {
-  console.log(req.body);
   var email = req.body.email;
+  var zip = req.body.zip;
 
   var error = null;
   if (!req.body.email)
@@ -21,7 +22,16 @@ router.post('/', function(req, res) {
   else if (!req.body.zip)
   	error = 'Please provide a zip code.';
   else {
-    // write to DB
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      client.query('INSERT INTO subscribers(email, zip) values($1, $2)', [req.body.email, req.body.zip], function(err, result) {
+        done();
+        if (err) {
+          console.error(err);
+          error = 'Something went wrong - please try again.';
+        }
+      });
+    });
+
     return res.render('success', {
     	title: 'Our Revolution',
     	email: req.body.email
